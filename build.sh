29efbefe8c68e9319150f5b64890afc529d94cc9
ilmo.euro <ilmo.euro@gmail.com>
2016-02-10 23:13:34 +0200

    ** Added build script

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+openscad -o raspirobot_body_shape.svg raspirobot_body_shape.scad
+openscad -o raspirobot_spacer1.stl raspirobot_spacer1.scad
+openscad -o raspirobot_spacer2.stl raspirobot_spacer2.scad
+
+# setup for laser cutter
+perl -pi -e 's/<svg width="\d+" height="\d+"/<svg width="120mm" height="70mm"/' \
+    raspirobot_body_shape.svg
+perl -pi -e 's/viewBox="[0-9 -]*"/viewBox="-60 -35 120 70"/' \
+    raspirobot_body_shape.svg
---
+++ raspirobot_body_shape.scad
@@ -0,0 +1,6 @@
+use <raspirobot.scad>
+
+rotate([0,0,90])
+    body_shape();
+
+    
